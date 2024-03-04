42acbca546e19b23abc9661ac1ef525175f22181
Amirreza Esmaeili <amirreza.77.esmaeili@gmail.com>
2024-03-04 13:17:41 -0800

    add basic configs for tmux and lvim

---
+++ lvim/.config/lvim/config.lua
@@ -0,0 +1,13 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- delay update diagnostics
+    update_in_insert = true,
+  }
+)
+
+vim.keymap.set("v", "<leader>lf", vim.lsp.buf.format, { remap = false, desc = "Code Format" })
---
+++ makefile
@@ -0,0 +1,5 @@
+essential:
+	stow -v -t $$HOME -R tmux lvim
+
+pre:
+	apt install stow
