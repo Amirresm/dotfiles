e09830f2a3ba5fbe9b6591232b36f48fc07a6f5d
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2024-03-10 03:37:00 -0700

    lvim: fix yank on wsl

--- lvim/.config/lvim/lua/core/options.lua
+++ lvim/.config/lvim/lua/core/options.lua
@@ -13,3 +13,22 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 		update_in_insert = true,
 	}
 )
+
+if vim.fn.has("wsl") == 1 then
+	if vim.fn.executable("win32yank.exe") == 0 then
+		print("win32yank not found, clipboard integration won't work")
+	else
+		vim.g.clipboard = {
+			name = "win32yank (wsl)",
+			copy = {
+				["+"] = 'win32yank.exe -i --crlf',
+				["*"] = 'win32yank.exe -i --crlf',
+			},
+			paste = {
+				["+"] = 'win32yank.exe -o --lf',
+				["*"] = 'win32yank.exe -o --lf',
+			},
+			cache_enabled = true
+		}
+	end
+end
