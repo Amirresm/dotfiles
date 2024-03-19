58a36ac799e0b289a7bf1fe17a792f5b4fbb4268
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2024-03-19 02:45:01 -0700

    some changes to lvim

--- lvim/.config/lvim/after/ftplugin/python.lua
+++ lvim/.config/lvim/after/ftplugin/python.lua
@@ -1,7 +1,16 @@
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup { { command = "flake8", filetypes = { "python" } } }
+linters.setup {
+	{
+		command = "flake8",
+		filetypes = { "python" },
+		extra_args = { "--max-line-length=120" }
+	}
+}
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-	{ name = "black" },
+	{
+		name = "black",
+		extra_args = { "--fast", "--preview", "--line-length", "80" },
+	},
 }
