a1bdb4271ddccffb7dc0b7fa826f6366f2610f87
amirresm <amirreza.77.esmaeili@gmail.com>
2024-10-11 10:00:12 -0700

    stuff

--- lvim/.config/lvim/lua/core/plugin.lua
+++ lvim/.config/lvim/lua/core/plugin.lua
@@ -13,14 +13,14 @@ lvim.plugins = {
 			require('neoscroll').setup({
 				mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
 					'<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-				hide_cursor = true,  -- Hide cursor while scrolling
-				stop_eof = true,     -- Stop at <EOF> when scrolling downwards
+				hide_cursor = true, -- Hide cursor while scrolling
+				stop_eof = true, -- Stop at <EOF> when scrolling downwards
 				use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
 				respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
 				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
 				easing_function = "sine", -- Default easing function
-				pre_hook = nil,      -- Function to run before the scrolling animation starts
-				post_hook = nil,     -- Function to run after the scrolling animation ends
+				pre_hook = nil,  -- Function to run before the scrolling animation starts
+				post_hook = nil, -- Function to run after the scrolling animation ends
 			})
 		end
 	},
@@ -42,11 +42,11 @@ lvim.plugins = {
 		"rmagatti/goto-preview",
 		config = function()
 			require('goto-preview').setup {
-				width = 120,     -- Width of the floating window
-				height = 25,     -- Height of the floating window
+				width = 120, -- Width of the floating window
+				height = 25, -- Height of the floating window
 				default_mappings = true, -- Bind default mappings
-				debug = false,   -- Print debug information
-				opacity = nil,   -- 0-100 opacity level of the floating window where 100 is fully transparent.
+				debug = false, -- Print debug information
+				opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
 				post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
 			}
 		end
@@ -59,5 +59,8 @@ lvim.plugins = {
 	},
 	{
 		"nvimtools/none-ls.nvim"
+	},
+	{
+		"sindrets/diffview.nvim"
 	}
 }
