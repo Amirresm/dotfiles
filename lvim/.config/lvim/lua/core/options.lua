65ab4a04ce70fd3c5584e3fa4d1a7d56b8ccaee6
Amirreza Esmaeili <amirreza.77.esmaeili@gmail.com>
2024-10-28 17:13:12 -0700

    add Difforig lvim

--- lvim/.config/lvim/lua/core/options.lua
+++ lvim/.config/lvim/lua/core/options.lua
@@ -38,3 +38,16 @@ if vim.fn.has("wsl") == 1 then
 		}
 	end
 end
+
+vim.api.nvim_create_user_command('Difforig', function()
+      local scratch_buffer = vim.api.nvim_create_buf(false, true)
+      local current_ft = vim.bo.filetype
+      vim.cmd('vertical sbuffer' .. scratch_buffer)
+      vim.bo[scratch_buffer].filetype = current_ft
+      vim.cmd('read ++edit #') -- load contents of previous buffer into scratch_buffer
+      vim.cmd.normal('1G"_d_') -- delete extra newline at top of scratch_buffer without overriding register
+      vim.cmd.diffthis() -- scratch_buffer
+      vim.cmd.wincmd('p')
+      vim.cmd.diffthis() -- current buffer
+end, {})
+
